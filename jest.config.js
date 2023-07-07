c81d40e7ddffa66a79b1e0a48447c1b09e437098
Pablo <loskylp@gmail.com>
2023-07-07 16:41:58 -0400

    Test Reports 1

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,11 @@ module.exports = {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    reporters: [
+      'default',
+      ['jest-junit',
+       {outputDirectory: 'reports', outputName: 'report.xml'},
+       ['github-actions', {silent: false}]],
+    ],
   };
